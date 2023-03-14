8fcbd57f10ff429a35d839e3c5c0d57517223f7e
galinakim <genrietta.kim@gmail.com>
2023-03-14 15:51:34 +0600

    created new function for autorization

--- AIS_Construction_company/AIS_Construction_company.cpp
+++ AIS_Construction_company/AIS_Construction_company.cpp
@@ -3,6 +3,7 @@
 #include <string>
 using namespace std;
 
+string user_login, user_password;
 
 struct Employee
 {
@@ -12,12 +13,34 @@ struct Employee
     string password;
 };
 
+void autorization(Employee arr[],int num_of_employee)
+{
+    starting:cout << "Enter your login >>> " << endl;
+    cin >> user_login;
+    cout << "Enter your password >>> " << endl;
+    cin >> user_password;
+    if (user_login == arr[num_of_employee].login && user_password == arr[num_of_employee].password)
+    {
+
+        cout << "DONE !!!";
+
+
+    }
+    else
+    {
+        cout << "Login or password is wrong , please try again " << endl;
+        goto starting;
+    }
+
+}
+
+
 int main()
 {
     //setlocale(LC_ALL, "Rus");
     ifstream in1("Employee_info.txt");
 
-    string str, user_login, user_password;
+    string str;
     int choice, next, i = 0;
     Employee arr[5]; // массив сотрудников (их пять)
     while (in1.peek() != EOF) /// заносим данные по каждому сотруднику в массив 
@@ -49,34 +72,14 @@ init_point: cin >> choice;
     }
     else
     {
-        system("cls");
+        system("cls"); // чтобы открывалось новое окно 
         switch (choice)
         {
-        case 1:
-        autorization:
-            cout << "Enter your login >>> " << endl;
-            cin >> user_login;
-            cout << "Enter your password >>> " << endl;
-            cin >> user_password;
-            if (user_login == arr[0].login && user_password == arr[0].password)
-            {
-
-                cout << "DONE !!!";
-
-
-            }
-            else
-            {
-                cout << "Login or password is wrong , please try again " << endl;
-                goto autorization;
-            }
-
-
-            break;
-        case 2: cout << "you are manager "; break;
-        case 3: cout << "you are worker "; break;
-        case 4: cout << "you are director "; break;
-        case 5: cout << "you are sale -manager "; break;
+        case 1: autorization(arr, 0); break;
+        case 2: autorization(arr, 1); break;
+        case 3: autorization(arr, 2); break;
+        case 4: autorization(arr, 3); break;
+        case 5: autorization(arr, 4); break;
         }
         //cout << "Enter your login >>> " ;
 
